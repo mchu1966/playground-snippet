db2a2e19b38afd207b42af2d1e447a040979c986
mchu1966 <mchu1966718@gmail.com>
2023-03-21 02:13:31 +0800

    add svg button for darkmode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
 	import CodeArea from 'components/codeArea.svelte';
+	import Svg from 'components/svg.svelte';
+
 	// fetch database to get the snippets (array of string of size N)
-	let mockSnippets: string[] = [
-		`// You can edit this code!
+	type snippet = {
+		name: string;
+		code: string;
+	};
+	let mockSnippets: snippet[] = [
+		{
+			name: 'Hello World',
+			code: `// You can edit this code!
 // Click here and start typing.
 package main
 
@@ -10,32 +18,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, 世界")
-}`,
-
-		`package main
-
-import "fmt"
-
-// fib returns a function that returns
-// successive Fibonacci numbers.
-func fib() func() int {
-	a, b := 0, 1
-	return func() int {
-		a, b = b, a+b
-		return a
-	}
-}
-
-func main() {
-	f := fib()
-	// Function calls are evaluated left-to-right.
-	fmt.Println(f(), f(), f(), f(), f())
-}
-`
+}`
+		}
 	];
 
 	function handleAddNew() {
-		mockSnippets = [...mockSnippets, ``];
+		mockSnippets = [...mockSnippets, { name: 'Type a name', code: `` }];
 	}
 
 	let darkMode = false;
@@ -49,12 +37,32 @@ func main() {
 </script>
 
 <div class="px-2 dark:bg-gray-400 h-screen">
-	<div class="p-2">
-		<button class="p-2 border shadow-md" on:click={handleSwitchDarkMode}>Switch</button>
-		<button class="p-2 border shadow-md" on:click={handleAddNew}>Add new</button>
+	<div class="p-2 content-center flex flex-row">
+		<button
+			class="transition duration-600 ease-in-out rounded-full p-2 border shadow-md"
+			on:click={handleAddNew}>Add new</button
+		>
+		<button class="transition duration-600 ease-in-out rounded-full p-2 border shadow-md"
+			>Save</button
+		>
+		<div class="grow" />
+		<button
+			class="transition duration-600 ease-in-out rounded-full border-collapse p-2 "
+			on:click={handleSwitchDarkMode}
+			>{#if darkMode}
+				<Svg name="sun" class="w-5 h-5 stroke-2 stroke-white fill-none" />
+			{:else}
+				<Svg name="moon" class="w-5 h-5 stroke-2 stroke-current fill-solid" />
+			{/if}</button
+		>
 	</div>
 	<!-- https://eternaldev.com/blog/5-ways-to-perform-for-loop-in-svelte-each-block/ -->
 	{#each mockSnippets as snippet, index}
-		<CodeArea code={snippet} />
+		<div class="p-4">
+			<div class="w-full flex flex-row">
+				<input bind:value={snippet.name} placeholder={snippet.name} class="w-full" />
+			</div>
+			<CodeArea code={snippet.code} />
+		</div>
 	{/each}
 </div>
