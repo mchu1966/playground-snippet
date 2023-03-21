1a07adf12ec89a402cd74446217e6a3691873972
mchu1966 <mchu1966718@gmail.com>
2023-03-21 15:58:10 +0800

    add header component

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,2 +1,39 @@
 <script lang="ts">
+	import Svg from 'components/svg.svelte';
+
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function handleAddNew() {
+		dispatch('addNew', { name: 'Type a name', code: `` });
+	}
+
+	let darkMode = false;
+	function handleSwitchDarkMode() {
+		darkMode = !darkMode;
+
+		darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark');
+	}
 </script>
+
+<div class="p-2 content-center flex flex-row">
+	<button
+		class="transition duration-600 ease-in-out rounded-full p-2 border shadow-md"
+		on:click={handleAddNew}>Add new</button
+	>
+	<button class="transition duration-600 ease-in-out rounded-full p-2 border shadow-md">Save</button
+	>
+	<div class="grow" />
+	<button
+		class="transition duration-600 ease-in-out rounded-full border-collapse p-2 "
+		on:click={handleSwitchDarkMode}
+		>{#if darkMode}
+			<Svg name="sun" class="w-5 h-5 stroke-2 stroke-white fill-none" />
+		{:else}
+			<Svg name="moon" class="w-5 h-5 stroke-2 stroke-current fill-solid" />
+		{/if}</button
+	>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import CodeArea from 'components/codeArea.svelte';
-	import Svg from 'components/svg.svelte';
-
+	import Header from 'components/header.svelte';
 	// fetch database to get the snippets (array of string of size N)
 	type snippet = {
 		name: string;
@@ -22,40 +21,13 @@ func main() {
 		}
 	];
 
-	function handleAddNew() {
-		mockSnippets = [...mockSnippets, { name: 'Type a name', code: `` }];
-	}
-
-	let darkMode = false;
-	function handleSwitchDarkMode() {
-		darkMode = !darkMode;
-
-		darkMode
-			? document.documentElement.classList.add('dark')
-			: document.documentElement.classList.remove('dark');
+	function handleAddNew(s: any) {
+		mockSnippets = [...mockSnippets, s.detail];
 	}
 </script>
 
 <div class="px-2 dark:bg-gray-400 h-screen">
-	<div class="p-2 content-center flex flex-row">
-		<button
-			class="transition duration-600 ease-in-out rounded-full p-2 border shadow-md"
-			on:click={handleAddNew}>Add new</button
-		>
-		<button class="transition duration-600 ease-in-out rounded-full p-2 border shadow-md"
-			>Save</button
-		>
-		<div class="grow" />
-		<button
-			class="transition duration-600 ease-in-out rounded-full border-collapse p-2 "
-			on:click={handleSwitchDarkMode}
-			>{#if darkMode}
-				<Svg name="sun" class="w-5 h-5 stroke-2 stroke-white fill-none" />
-			{:else}
-				<Svg name="moon" class="w-5 h-5 stroke-2 stroke-current fill-solid" />
-			{/if}</button
-		>
-	</div>
+	<Header on:addNew={handleAddNew} />
 	<!-- https://eternaldev.com/blog/5-ways-to-perform-for-loop-in-svelte-each-block/ -->
 	{#each mockSnippets as snippet, index}
 		<div class="p-4">
